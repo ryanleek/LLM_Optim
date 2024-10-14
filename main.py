efea9637b919ef590bdf542a6d85e03f0c4b82ee
Kwangwon <30751720+ryanleek@users.noreply.github.com>
2024-10-15 08:42:55 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+import json
 from datetime import datetime
 
 import pytz
 from supabase import create_client
 from dotenv import load_dotenv
+from openai import OpenAI
 from fasthtml.common import*
 
 
@@ -12,80 +14,119 @@ load_dotenv()
 TIMESTAMP_FMT = "%Y-%m-%d %I:%M:%S %p KST"
 
 supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 app, rt = fast_app()
+msgList = []
 
 def get_kst_time():
     kst_tz = pytz.timezone("Asia/Seoul")
     return datetime.now(kst_tz)
 
-def add_message(name, message):
-    timestamp = get_kst_time().strftime(TIMESTAMP_FMT)
-    supabase.table("messages").insert(
-        {"name": name, "message": message, "timestamp": timestamp}
+def add_task(context, data):
+    #timestamp = get_kst_time().strftime(TIMESTAMP_FMT)
+    supabase.table("task").insert(
+        {"context": context, "data": data}
     ).execute()
 
-def get_messages():
+def get_prompts():
     response = (
-        supabase.table("messages").select("*").order("id", desc=True).execute()
+        supabase.table("prompts").select("*").order("id", desc=True).execute()
     )
     return response.data
 
-def render_message(entry):
-    return Article(
-        Header(f"Name: {entry['name']}"),
-        P(entry['message']),
-        Footer(Small(Em(f"Posted: {entry['timestamp']}"))),
+def get_prompt(prompt, context):
+    response = (
+        supabase.table(prompt).select("*").eq("name", context).execute()
+    )
+    return response.data[0]['content']
+
+def render_process(entry):
+    print(entry)
+    return Details(
+        Summary(
+            entry['name'],
+            role="button",
+            cls="secondary",
+            id="process",
+        ),
+        P(entry['content']),
     )
 
-def render_message_list():
-    messages = get_messages()
+def render_process_list():
+    prompts = get_prompts()
 
     return Div(
-        *[render_message(entry) for entry in messages],
-        id="message-list",
+        *[render_process(entry) for entry in prompts],
+        id="process-list",
+        style="overflow: scroll"
     )
 
 def render_content():
-    form = Form(
+    header = Form(
         Fieldset(
-            Input(
+            Label(
+                "Context",
+                Input(
                 type="text",
-                name="name",
-                placeholder="Name",
+                name="context",
+                value=get_prompt("contexts", "food"),
                 required=True,
-                maxlength=15,
+                ),
             ),
+            Label(
+                "Data",
+                Grid(
+                    Select(
+                        Option(
+                            "food",
+                        ),
+                        name="data",
+                        required=True,
+                    ),
+                    Div(),
+                    Div(),
+                    Button("Submit", type="submit"),
+                ),
+            ),
+            method="post",
+            hx_post="/submit-context-data",
+        ),
+    ),
+
+    footer = Form(
+        Group(
             Input(
                 type="text",
                 name="message",
-                placeholder="Message",
-                required=True,
-                maxlength=50,
             ),
-            Button("Submit", type="submit"),
-            #role="group",
+            Button("Send"),
         ),
         method="post",
-        hx_post="/submit-message",
-        hx_target="#message-list",
+        hx_post="/submit-preference",
+        hx_target="#process-list",
         hx_swap="outerHTML",
         hx_on__after_request="this.reset()",
-    )
+    ),
 
     return Div(
-        P(Em("Write something nice!")),
-        form,
+        header,
         Hr(),
-        render_message_list()
+        render_process_list(),
+        Hr(),
+        footer,
     )
 
 @rt("/")
 def get():
     return Titled("LLM Optimizer", render_content())
 
-@rt("/submit-message", methods=["POST"])
-def post(name: str, message:str):
-    add_message(name, message)
-    return render_message_list()
+@rt("/submit-context-data", methods=["POST"])
+def post(context: str, data: str):
+    add_task(context, data)
+
+@rt("/submit-preference", methods=["POST"])
+def post(message: str):
+    #add_message(message)
+    return render_process_list()
 
 serve()
